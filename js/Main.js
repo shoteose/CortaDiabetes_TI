2eab3a2b96a229972cdbabf39708e57c324ee2a2
Paulo Novo <143038290+shoteose@users.noreply.github.com>
2025-03-18 14:56:28 +0000

    update

--- js/Main.js
+++ js/Main.js
@@ -140,10 +140,7 @@ function gotSpeech() {
             if (speechRec.resultString == "jogar") {
                 estadoJogo = 1;
 
-                objetos = [];
-                for (let i = 0; i < 5; i++) {
-                    objetos.push(new Objeto());
-                }
+                initJogo();
 
             } else if (speechRec.resultString == "opções") {
                 estadoJogo = 2;
@@ -165,10 +162,7 @@ function gotSpeech() {
                 score = 0;
                 vidas = 3;
 
-                objetos = [];
-                for (let i = 0; i < 5; i++) {
-                    objetos.push(new Objeto());
-                }
+                initJogo();
                 console.log("reiniciei");
 
             } else if (speechRec.resultString == "voltar" || speechRec.resultString == "menu") {
@@ -179,6 +173,13 @@ function gotSpeech() {
 
             }
         }
+    }
+}
+
+function initJogo(){
+    objetos = [];
+    for (let i = 0; i < 5; i++) {
+        objetos.push(new Objeto());
     }
 }
 
